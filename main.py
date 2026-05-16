13f3e558371ccb777cc082c1a35cedc24ddf591d
Lotikys <egorp4700@mail.ru>
2026-05-16 18:42:48 +0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 import spritePro as s
 
-bg =s.Sprite("background.jfif",pos = (200,300),size=(400,600))
-player = s.Sprite("bird.png",pos =(100,300),size = (50,50))
-s.run(size=(400,600),title="Flappy Bird")
 
+class GameScene(s.Scene):
+    def __init__(self):
+        super().__init__()
+        self.bg = s.Sprite("background.jfif",pos=s.WH_C,size=s.WH,scene =self)
+        self.player = s.Sprite("bird.png",pos =(100,300),size = (50,50), scene = self)
+        self.player_body = s.add_physics(self.player,s.PhysicsConfig(bounce=1.0))
+        s.physics.set_gravity(980)
+if __name__ == '__main__':
+    s.run(scene=GameScene,size=(400,600),title ="Flappy Bird")
